9b333f7d1d5bb40f7f86d52f1b4c307067748316
Ken Hernando <kennethjohnhernando@gmail.com>
2020-02-02 22:21:37 +0800

    INIT: initialize project

---
+++ tic-tac-toe/src/app/player-names/player-names.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-names',
+  templateUrl: './player-names.component.html',
+  styleUrls: ['./player-names.component.scss']
+})
+export class PlayerNamesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
